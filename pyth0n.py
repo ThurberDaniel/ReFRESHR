8c00b9e9c85061e00f95a3288257f4c022e580d4
ThurberDaniel <phxburg@gmail.com>
2021-10-12 12:34:42 -0600

    tuesday

--- pyth0n.py
+++ pyth0n.py
@@ -1,4 +1,10 @@
 name = "Kim"
-
 age = 44
 sentence = f"Hi my name is {name} and I am {44} years old!"
+print(sentence)
+# //////////////////////////////////
+Birthyear = 1900
+if 2000 <= Birthyear < 2100:
+    print("Welcome to the 21st century")
+else:
+    print("Your born before the 21st")
